53acf0bc43542899b9ff13cd3055f70f6e4eef7e
Audrey <viriotaudrey@gmail.com>
2019-05-17 18:40:41 +0200

    Modif accieul suite

--- script/accueil.js
+++ script/accueil.js
@@ -2,9 +2,13 @@ window.onload = function(){
     
     var btnInsta = document.getElementById("insta");
     var btnFb = document.getElementById("fb");
+    var btnActeur = document.getElementById("acteur");
+    var btnEvent = document.getElementById("event");
     
     btnInsta.addEventListener("click", clicInsta);
     btnFb.addEventListener("click", clicFb);
+    btnActeur.addEventListener("click", clicActeur);
+    btnEvent.addEventListener("click", clicEvent);
     
     function clicInsta(){
         window.open("https://www.instagram.com/initiative_region_propre/");
@@ -12,6 +16,14 @@ window.onload = function(){
     
     function clicFb(){
         window.open("https://www.facebook.com/regionpropre/");
+    }
+    
+    function clicEvent(){
+        window.open("events.html");
+    }
+    
+    function clicActeur(){
+        window.onload("index.html");
     }
     
 }
